0956b71deb8640e523eea025689627253b0f6108
Emine Çetin <80969567+eminecetin@users.noreply.github.com>
2023-08-09 11:09:18 +0300

    Proje dosyası ekle.

---
+++ is_takip_proje/Formlar/FrmDepartmanlar.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace is_takip_proje.Formlar
+{
+    partial class FrmDepartmanlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDepartmanlar));
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnEkle = new DevExpress.XtraEditors.SimpleButton();
+            this.Btnlistele = new DevExpress.XtraEditors.SimpleButton();
+            this.TxtAd = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.TxtID = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtAd.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtID.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(-1, 1);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(692, 342);
+            this.gridControl1.TabIndex = 4;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.simpleButton4);
+            this.groupControl1.Controls.Add(this.BtnEkle);
+            this.groupControl1.Controls.Add(this.Btnlistele);
+            this.groupControl1.Controls.Add(this.TxtAd);
+            this.groupControl1.Controls.Add(this.labelControl2);
+            this.groupControl1.Controls.Add(this.simpleButton1);
+            this.groupControl1.Controls.Add(this.TxtID);
+            this.groupControl1.Controls.Add(this.labelControl1);
+            this.groupControl1.Location = new System.Drawing.Point(714, 1);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(300, 342);
+            this.groupControl1.TabIndex = 5;
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton4.ImageOptions.Image")));
+            this.simpleButton4.Location = new System.Drawing.Point(183, 296);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(112, 34);
+            this.simpleButton4.TabIndex = 7;
+            this.simpleButton4.Text = "Güncelle";
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
+            this.BtnEkle.Location = new System.Drawing.Point(183, 216);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(112, 34);
+            this.BtnEkle.TabIndex = 6;
+            this.BtnEkle.Text = "Ekle";
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // Btnlistele
+            // 
+            this.Btnlistele.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("Btnlistele.ImageOptions.Image")));
+            this.Btnlistele.Location = new System.Drawing.Point(184, 176);
+            this.Btnlistele.Name = "Btnlistele";
+            this.Btnlistele.Size = new System.Drawing.Size(112, 34);
+            this.Btnlistele.TabIndex = 5;
+            this.Btnlistele.Text = "Listele";
+            this.Btnlistele.Click += new System.EventHandler(this.Btnlistele_Click);
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(145, 131);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(150, 26);
+            this.TxtAd.TabIndex = 4;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(22, 138);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(113, 19);
+            this.labelControl2.TabIndex = 3;
+            this.labelControl2.Text = "Departman Adı:";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(183, 256);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(112, 34);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Sil";
+            // 
+            // TxtID
+            // 
+            this.TxtID.Location = new System.Drawing.Point(145, 51);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(150, 26);
+            this.TxtID.TabIndex = 1;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(22, 54);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(106, 19);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Departman ID:";
+            // 
+            // FrmDepartmanlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 344);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "FrmDepartmanlar";
+            this.Text = "Departmanlar";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtAd.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtID.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.TextEdit TxtAd;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.TextEdit TxtID;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private DevExpress.XtraEditors.SimpleButton BtnEkle;
+        private DevExpress.XtraEditors.SimpleButton Btnlistele;
+    }
+}
---
+++ is_takip_proje/Formlar/FrmDepartmanlar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using is_takip_proje.Entity;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace is_takip_proje.Formlar
+{
+    public partial class FrmDepartmanlar : Form
+    {
+        public FrmDepartmanlar()
+        {
+            InitializeComponent();
+        }
+
+       
+        void Listele()
+        {
+            DbisTakipEntities db=new DbisTakipEntities();
+            var degerler=(from x in db.TblDepartmanlar
+                         select new
+                         {
+                             x.ID,
+                             x.Ad
+                         }).ToList();
+            gridControl1.DataSource = db.TblDepartmanlar.ToList();
+        }
+        private void Btnlistele_Click(object sender, EventArgs e)
+        {
+            Listele();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            TblDepartmanlar t=new TblDepartmanlar();
+            t.Ad = TxtAd.Text;
+        }
+    }
+}
